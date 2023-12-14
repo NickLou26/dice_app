79b1f1a1965bea66bfb95215c783d00e988d3f14
NickLou26 <nickolaslou@outlook.com>
2023-12-14 17:49:13 -0500

    roll dice function with randomizer

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -8,13 +12,31 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 1;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Image.asset(
-          'assets/images/dice-1.png',
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
         ),
       ],
     );
